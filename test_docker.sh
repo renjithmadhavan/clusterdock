08da720f4b118b68691f6337655685a3ac9d06c5
renjithmadhavan <renjithmadhavan@gmail.com>
2017-03-20 04:03:09 +0000

    c1

--- test_docker.sh
+++ test_docker.sh
@@ -1,3 +1,13 @@
 docker run hello-world
 docker ps
 docker images
+
+# https://docs.oracle.com/cd/E56133_01/1.2.0/tutorials/install-hadoop.html
+# download and install java 1.8
+# downlaod cloudera jdbc drivers for impala
+# download client configuration files from cm
+# download apache hive 1.1.0 for beeline
+# set the classpath export CLASSPATH=
+# set JAVA_HOME
+# jdbc:hive2://myhost.example.com:21050/;auth=noSasl
+
